9299b3fd06363c6390c18ef963a11c1f6f701c37
Clevermike <64679578+mikky00B@users.noreply.github.com>
2024-12-16 01:52:48 +0100

    Update urls.py

--- Xinsight/urls.py
+++ Xinsight/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("analytics.urls")),
+    path("fhf", include("analytics.urls")),
 ]
